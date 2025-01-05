423db5e3ec2e0933454c7f3a1d97b8ee05150ad1
K32ar <cezarrusu20.000@gmail.com>
2025-01-06 00:53:17 +0200

    Implemented vectors and strings (partial ig)

--- src/ka.h
+++ src/ka.h
@@ -1,34 +1,141 @@
 #ifndef KA_H
 #define KA_H
 
+#include <stdlib.h>
+#include <string.h>
 #include "ke.h"
 
-#include <stdint.h>
-
 typedef struct ka_vector_struct {
-  void *items;
-  size_t item_size;
-  size_t item_count;
-  size_t dimension;
-} ka_vector_t;
+  void *data;
+  unsigned long long int count;
+} ka_vector;
 
-ka_vector_t ka_init_vector(void *items, size_t item_size, size_t *item_counts, size_t dimension_count);
-void ka_free_vector(ka_vector_t *vector);
+#define KA_VECTOR_ALLOC(vec, num, type) { \
+    (vec).data = NULL, (vec).count = 0; \
+    if ((num) > 0) { \
+      (vec).data = calloc((num), sizeof((type))); \
+      if ((vec).data == NULL) KE_ERROR("ERROR: KA failed allocating memory."); \
+      (vec).count = (num); \
+      ke_push_elem(&(vec), ka_vector_free); \
+    } \
+  }
+
+#define KA_VECTOR_REALLOC(vec, num, type) { \
+    if ((num) > 0) { \
+      void *temp = realloc((vec).data, (num) * sizeof((type))); \
+      if (temp == NULL) KE_ERROR("ERROR: KA failed re-allocating memory."); \
+      (vec).data = temp; \
+      if ((num) > (vec).count) memset((vec).data + (vec).count * sizeof((type)), 0, ((num) - (vec).count) * sizeof((type))); \
+      (vec).count = (num); \
+    } \
+  }
+
+#define KA_VECTOR_FREE(vec) { \
+    if ((vec).count > 0) free((vec).data); \
+    (vec).data = NULL; \
+    (vec).count = 0; \
+  }
+
+void ka_vector_free(void *);
+
+#ifdef KA_IMPL
+
+void ka_vector_free(void *vec) { KA_VECTOR_FREE(*((ka_vector *)vec)) }
+
+#endif // KA_IMPL
+
+
+typedef struct ka_string_struct {
+  char *data;
+  unsigned long long int space, length;
+} ka_string;
+
+#define KA_STRING_ALLOC(str, len) { \
+    (str).data = NULL, (str).space = (str).length = 0; \
+    if ((len) > 0) { \
+      (str).data = calloc((len), sizeof(char)); \
+      if ((str).data == NULL) KE_ERROR("ERROR: KA failed allocating memory."); \
+      (str).space = (len); \
+      ke_push_elem(&(str), ka_string_free); \
+    } \
+  }
+#define KA_STRING_REALLOC(str, len) { \
+    if ((len) > 0) { \
+      void *temp = realloc((str).data, (len) * sizeof(char)); \
+      if (temp == NULL) KE_ERROR("ERROR: KA failed re-allocating memory."); \
+      (str).data = temp; \
+      if ((len) > (str).space) memset((str).data + (str).space * sizeof(char), '\0', ((len) - (str).space) * sizeof(char)); \
+      else { \
+        if ((len) <= (str).length) { \
+          (str).length = (len) - 1; \
+          (str).data[(str).length] = '\0'; \
+        } \
+      } \
+      (str).space = (len); \
+    } \
+  }
+#define KA_STRING_FREE(str) { \
+    if ((str).space > 0) free((str).data); \
+    (str).data = NULL; \
+    (str).space = (str).length = 0; \
+  }
+
+void ka_string_free(void *);
+
+#ifdef KA_IMPL
+
+void ka_string_free(void *str) { KA_STRING_FREE(*((ka_string *)str)) }
+
+#endif // KA_IMPL
+
+#define KA_BEST_SIZE(size) { \
+    unsigned long long int power = 0; \
+    if ((size) < 64) while ((size) > power) power += 8; \
+    else if ((size <= 128)) while ((size) > power) power += 16; \
+    else while ((size) > power) power += 64; \
+    (size) = power; \
+  }
+
+#define KA_STRING_CPY(dst, src) { \
+    unsigned long long int src_size = strlen((src)); \
+    if ((dst).space <= src_size + 1) { \
+      unsigned long long int new_size = src_size + 1; KA_BEST_SIZE(new_size) \
+      KA_STRING_REALLOC(dst, new_size) \
+      } \
+    \
+    unsigned long long int i; \
+    for (i = 0; i < src_size; i++) (dst).data[i] = (src)[i]; \
+    memset((dst).data + i * sizeof(char), '\0', ((dst).space - i) * sizeof(char)); \
+    (dst).length = src_size; \
+  }
+
+#define KA_STRING_CAT(dst, src) { \
+    unsigned long long int src_size = strlen((src)); \
+    if ((dst).space <= (dst).length + src_size + 1) { \
+      unsigned long long int new_size = (dst).length + src_size + 1; KA_BEST_SIZE(new_size) \
+      KA_STRING_REALLOC(dst, new_size) \
+      } \
+    \
+    unsigned long long int i; \
+    for (i = (dst).length; i < (dst).length + src_size; i++) (dst).data[i] = (src)[i - (dst).length]; \
+    memset((dst).data + i * sizeof(char), '\0', ((dst).space - i) * sizeof(char)); \
+    (dst).length += src_size; \
+  }
+
+#define KA_STRING_SLICE(dst, src, start, size) { \
+    if ((dst).space <= (size) + 1) { \
+      unsigned long long int new_size = (size) + 1; KA_BEST_SIZE(new_size) \
+      KA_STRING_REALLOC(dst, new_size) \
+      } \
+    \
+    unsigned long long int i; \
+    for (i = 0; i < (size); i++) (dst).data[i] = (src)[i + (start)]; \
+    memset((dst).data + i * sizeof(char), '\0', ((dst).space - i) * sizeof(char)); \
+    (dst).length = (size); \
+  }
 
 #ifdef KA_IMPL
 #undef KA_IMPL
-
-ka_vector_t ka_init_vector(void *items, size_t item_size, size_t *item_counts, size_t dimension_count) {
-  ka_vector_t vector = { 0 };
-
-  if (item_counts > 0) {
-  }
-
-  return vector;
-}
-void ka_free_vector(ka_vector_t *vector) {
-}
-
 #endif // KA_IMPL
 
 #endif // KA_H
--- src/main.c
+++ src/main.c
@@ -4,9 +4,97 @@
 #define KA_IMPL
 #include "ka.h"
 
+#include <stdio.h>
+
 int main(void) {
   ke_init();
   ke_push_scope(true);
+
+/* test_vec
+  ka_vector test_vec = { 0 };
+
+  KA_VECTOR_ALLOC(test_vec, 100, (unsigned long long int)0)
+
+  #define TEST_VEC_PRINT() { \
+      unsigned long long int i = 0; \
+      for (i = 0; i < test_vec.count; i++) printf("%llu: %llu\n", i, ((unsigned long long int *)test_vec.data)[i]); \
+      printf("\n"); \
+    }
+  #define TEST_VEC_FILL(val) { \
+      unsigned long long int i = 0; \
+      for (i = 0; i < test_vec.count; i++) ((unsigned long long int *)test_vec.data)[i] = (val); \
+    }
+
+  TEST_VEC_PRINT()
+
+  TEST_VEC_FILL(2024)
+
+  TEST_VEC_PRINT()
+
+  KA_VECTOR_REALLOC(test_vec, 1000, (unsigned long long int)0)
+
+  TEST_VEC_PRINT()
+
+  TEST_VEC_FILL(2025)
+
+  TEST_VEC_PRINT()
+
+  KA_VECTOR_REALLOC(test_vec, 10, (unsigned long long int)0)
+
+  TEST_VEC_PRINT()
+
+  TEST_VEC_FILL(2026)
+
+  TEST_VEC_PRINT()
+
+  KA_VECTOR_FREE(test_vec) // -- optional letsss goooo
+
+  TEST_VEC_PRINT()
+*/
+
+/* test_str
+  ka_string test_str = { 0 };
+
+  KA_STRING_ALLOC(test_str, 8)
+
+  printf("%lld: %lld: %s\n", test_str.space, test_str.length, test_str.data);
+
+  KA_STRING_CPY(test_str, "Test Test 1");
+
+  printf("%lld: %lld: %s\n", test_str.space, test_str.length, test_str.data);
+
+  KA_STRING_CPY(test_str, "");
+
+  printf("%lld: %lld: %s\n", test_str.space, test_str.length, test_str.data);
+
+  {
+    KA_STRING_CAT(test_str, "Test Test ");
+    KA_STRING_CAT(test_str, "1");
+    printf("%lld: %lld: %s\n", test_str.space, test_str.length, test_str.data);
+  }
+
+  KA_STRING_SLICE(test_str, "Test Test 1 Test Test 12 Test Test 123 Test Test 1234 Test Test 12345 Test Test 123456", 12, 12);
+
+  printf("%lld: %lld: %s\n", test_str.space, test_str.length, test_str.data);
+
+
+  ka_string test_str2 = { 0 };
+
+  KA_STRING_ALLOC(test_str2, 8)
+
+  KA_STRING_CPY(test_str2, "Test Test 1234");
+
+  KA_STRING_CAT(test_str2, test_str.data)
+  KA_STRING_CAT(test_str, test_str2.data)
+
+  printf("%lld: %lld: %s\n", test_str.space, test_str.length, test_str.data);
+  printf("%lld: %lld: %s\n", test_str2.space, test_str2.length, test_str2.data);
+
+  KA_STRING_FREE(test_str2)
+
+
+  KA_STRING_FREE(test_str)
+*/
 
   ke_pop_scope();
   ke_free();
